9b7e5c0e169f86c769cb310f5fdf7b037bd883a5
Brandon Namphong <brandon.namphong@gmail.com>
2021-07-05 20:20:46 -0500

    initial website

---
+++ handle.js
@@ -0,0 +1,11 @@
+/*window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 50) {
+    document.getElementById("title").className = "slideUp";
+    document.getElementsByClassName("para").className = "slideUp";
+  } else {
+    document.getElementById("title").className = "";
+    document.getElementsByClassName("para").className = "";
+  }
+}*/
